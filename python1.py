1380776aece9dbe094ca5a3c2341967deeccf09a
APEKSHASHIRSATH410 <99476734+APEKSHASHIRSATH410@users.noreply.github.com>
2022-03-11 14:58:30 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,4 +1,8 @@
-num1 = 1.5
-num2 = 6.3
+num1 = 15
+num2 = 12
+  
+# Adding two nos
 sum = num1 + num2
-print('sum')
+  
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
